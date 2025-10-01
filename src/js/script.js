92a30b3585d1f35f86391134ffe72fc6e71dcad8
princie <tiana.mury@epitech.eu>
2025-10-01 13:42:01 +0200

    [ADD] Amélioration du Portfolio

--- src/js/script.js
+++ src/js/script.js
@@ -44,3 +44,38 @@ themeToggle.addEventListener('click', () => {
 // Charger le thème sauvegardé
 const savedTheme = localStorage.getItem('theme') || 'light';
 setTheme(savedTheme);
+// Thème clair/sombre
+const toggleBtn = document.getElementById('theme-toggle');
+toggleBtn.addEventListener('click', () => {
+  document.documentElement.classList.toggle('dark');
+  if (document.documentElement.classList.contains('dark')) {
+    localStorage.theme = 'dark';
+    toggleBtn.textContent = '☀️';
+  } else {
+    localStorage.theme = 'light';
+    toggleBtn.textContent = '🌙';
+  }
+});
+
+
+const grid = document.getElementById("skills-grid");
+
+skills.forEach(skill => {
+  const card = document.createElement("div");
+  card.className = "flip-card";
+
+  card.innerHTML = `
+    <div class="flip-inner shadow-lg cursor-pointer">
+      <!-- Recto -->
+      <div class="flip-front bg-gradient-to-r ${skill.color}">
+        <div class="text-4xl mb-4">${skill.icon}</div>
+        <h3 class="font-semibold text-lg">${skill.name}</h3>
+      </div>
+      <!-- Verso -->
+      <div class="flip-back rounded-2xl">
+        <p>${skill.desc}</p>
+      </div>
+    </div>
+  `;
+  grid.appendChild(card);
+});
